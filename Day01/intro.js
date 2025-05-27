93202c10f4583f7ffff7ef8619ef54412fab67a0
Abdi-01 <abdialghi@gmail.com>
2025-05-27 12:08:38 +0700

    Day01

---
+++ Day01/intro.js
@@ -0,0 +1,13 @@
+// Single line comment
+// Intro to variable
+
+/*
+Multiple line comment :
+Pengenalan dengan variable pada javascript
+*/
+
+// Uji coba funsgi bawaan javascript
+// console.log(data) : fungsi yang ditujukan untuk menampilkan data di terminal
+console.log("Module 01 Fundamental"); // Menampilkan tipe data string
+console.log(typeof 12); // Menampilkan tipe data angka/number
+console.log(typeof "22");
